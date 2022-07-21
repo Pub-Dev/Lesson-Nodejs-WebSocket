00d083d6527d043a71523ab87e8e248e4ac6212f
Rafael Kenji Nagai <rafakenji23@gmail.com>
2022-07-21 20:37:56 -0300

    feat: created websocket server

---
+++ app-ws.js
@@ -0,0 +1,80 @@
+const WebSocket = require('ws');
+
+var playersOn = [];
+
+function onError(ws, err){
+	console.error(`onError: ${err.message}`);
+}
+
+function onMessage(wss, ws, data) {
+	var obj = JSON.parse(data.toString());
+	wss.clients.forEach(function each(client){
+		if(client === ws && client.readyState === WebSocket.OPEN){
+			addPlayer(client, obj);
+		}
+		if(client !== ws && client.readyState === WebSocket.OPEN){			
+			client.send(JSON.stringify(obj));
+		}
+	});
+}
+
+function addPlayer(client, data){
+	if(data['action'] === 'ALLY_INVOCATION') {
+		playersOn.push({
+			"client": client,
+			"id": data['id']
+		})
+	}
+}
+
+function onDisconnect(wss, ws) {
+	console.log(`onDisconnect`);
+
+	playersOn.forEach(function playersOnMethod(player){
+		var validPlayer = false;
+		wss.clients.forEach(function each(client){
+			if(client === player.client){
+				validPlayer = true;
+				return;
+			}
+		});
+		if(!validPlayer) {
+			wss.clients.forEach(function each(client){
+				if(client !== player.client){
+					client.send(JSON.stringify({
+						"id":player.id,
+						"action":"DISCONNECT",
+						"direction":"RIGHT",
+					}));
+				}
+			});
+			playersOn = playersOn.filter(function(obj){
+				return player !== obj;
+			});
+		}
+	});
+
+	
+}
+
+function onConnection(wss, ws, req) {
+	ws.on('message', data => onMessage(wss, ws, data));
+	ws.on('error', error => onError(ws, error));
+	ws.on('close', function close() { 
+		onDisconnect(wss, ws);
+	});
+	
+	console.log('onConnection');
+}
+
+module.exports = (server) => {
+	const wss = new WebSocket.Server({
+		server
+	});
+	wss.on('connection', function conn(ws, req){
+		onConnection(wss, ws, req);
+	});
+	
+	console.log('App WebSocket Server is Running!');
+	return wss;
+}
